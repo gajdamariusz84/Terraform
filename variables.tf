f320fe170b193b53aa1110450cc9b1ec37b7e251
Mariusz <gajdamariusz84@gmail.com>
2023-06-06 19:00:57 +0000

    init commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "aws_key_name" {
+  type        = string
+  description = "Nawa klucza dodanego w AWS"
+}
+
+variable "ssh_key_path" {
+  description = "Sciezka do klucza SSH"
+}
+
+variable "bucket_name" {}
